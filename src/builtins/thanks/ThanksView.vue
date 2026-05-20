12efed5f65f1e44f19b93d8628828f525e6c7818
brendenlake <brenden.lake@gmail.com>
2026-05-20 12:49:34 -0400

    fix: correct Prolific submission URL in ThanksView component

--- src/builtins/thanks/ThanksView.vue
+++ src/builtins/thanks/ThanksView.vue
@@ -118,7 +118,7 @@ onMounted(() => {
               <Button
                 variant="default"
                 as="a"
-                :href="`https://app.prolific.co/submissions/complete?cc=${completionCode}`"
+                :href="`https://app.prolific.com/submissions/complete?cc=${completionCode}`"
               >
                 Submit my work to Prolific
                 <i-fa6-solid-arrow-right />
